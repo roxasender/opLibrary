d71f2952019e4014ac1e9c7806cf2fef65c2be78
Phuoc <90246144+uedNguyenHuuPhuoc@users.noreply.github.com>
2022-11-11 09:30:11 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
 import Index from "./components/index/index";
 import IndexNav from "./components/navbars/indexnav";
 import Login from "./components/auth/login/login";
+import Register from "./components/auth/register/register";
 
 function App() {
   return (
     <Router>
-        <div>
-          <Routes>
-            <Route exact path="/" element={<Index />} />
-			<Route exact path="/login" element={<Login />} />
-          </Routes>
-        </div>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Index />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
